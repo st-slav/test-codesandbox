0659792a7a8be2ef7ef5a49b1bd362973ddf03c4
st-slav <st.mam_@hotmail.com>
2021-04-07 15:12:44 +0300

    test

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import styled from 'styled-components'
-import { styles } from '../constants'
+import styled from "styled-components";
+import { styles } from "../constants";
 
 const layout = `
   max-width: ${styles.LAYOUT_WIDTH}px;
@@ -9,13 +9,15 @@ const layout = `
   margin: 0 auto;
   color: #07001F;
   font-family: sans-serif;
-`
+`;
 
 const LayoutMain = styled.main`
   ${layout}
   display: flex;
-`
-const LayoutHeader = styled.header`${layout}`
+`;
+const LayoutHeader = styled.header`
+  ${layout}
+`;
 const LayoutFooter = styled.footer`
   ${layout}
 
@@ -23,23 +25,27 @@ const LayoutFooter = styled.footer`
     content: "©";
     padding-right: 5px;
   }
-`
+`;
 export const LayoutHr = styled.hr`
   min-width: ${styles.LAYOUT_MIN_WIDTH}px;
-  height: 1px; 
+  height: 1px;
   overflow: hidden;
   background-color: ${styles.HR_COLOR};
   border: none;
   margin: 0;
-`
+`;
 
 const Layout = ({ children }) => (
   <>
-    <LayoutHeader><h1>Список гостей</h1></LayoutHeader>
+    <LayoutHeader>
+      <h1>Список животных гостей</h1>
+    </LayoutHeader>
     <LayoutHr />
     <LayoutMain>{children}</LayoutMain>
     <LayoutHr />
-    <LayoutFooter><p>{new Date().getFullYear()}</p></LayoutFooter>
+    <LayoutFooter>
+      <p>{new Date().getFullYear()}</p>
+    </LayoutFooter>
   </>
 );
 
